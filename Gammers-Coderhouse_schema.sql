6779ce9e009fea92614920b35313b4dc8ee937c1
SantiagoRibot96 <85650377+santiagoribot@users.noreply.github.com>
2023-09-25 22:22:58 -0300

    clase 3

---
+++ Gammers-Coderhouse_schema.sql
@@ -0,0 +1,3 @@
+drop schema if exists gammer;
+create schema if not exists gammer;
+use gammer;
---
+++ clase3.sql
@@ -0,0 +1,11 @@
+use gammer;
+
+/* 
+select: columnas de las tablas que quiero ver
+from: el nombre de la tabla que quiero consultar
+*/
+
+select 	id_commentary,
+		id_system_user,
+        commentary
+from 	commentary;
