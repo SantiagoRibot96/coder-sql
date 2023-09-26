3546c602e418cc1c1899992ee2e53eb8454bcf0d
SantiagoRibot96 <85650377+santiagoribot@users.noreply.github.com>
2023-09-25 22:23:08 -0300

    clase 3

--- clase3.sql
+++ clase3.sql
@@ -1,11 +1,19 @@
 use gammer;
 
 /* 
-select: columnas de las tablas que quiero ver
+select: columnas de las tablas que quiero ver. Colocando * puedo ver todas las columnas sin necesidad de describirlas
 from: el nombre de la tabla que quiero consultar
+where: te ayuda a filtrar alguna columna
 */
 
 select 	id_commentary,
 		id_system_user,
         commentary
 from 	commentary;
+
+select 	*
+from 	commentary;
+
+select 	*
+from 	game
+where 	id_game < 8;
